d8a693adc2e74af02bd43f181a40207754fac7df
mag@planetfwd.com <mag@planetfwd.com>
2021-07-27 12:34:52 -0700

    implement basic SfGrid

--- WeatherDashboard/Client/Program.cs
+++ WeatherDashboard/Client/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace WeatherDashboard.Client
@@ -18,6 +15,8 @@ namespace WeatherDashboard.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
         }
