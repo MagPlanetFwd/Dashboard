8c64dafd6eceadb12263ad90d737316c5d8b9251
Matuts Jiraboondilok <matuts.j@gmail.com>
2021-07-27 10:25:50 -0700

    extract logic to code behind

---
+++ WeatherDashboard/Client/Pages/Forecast.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WeatherDashboard.Client.ViewModels;
+
+namespace WeatherDashboard.Client.Pages
+{
+    public class ForecastBase: ComponentBase
+    {
+        protected WeatherGridRow[] Forecasts { get; private set; }
+
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Forecasts = await Client.GetFromJsonAsync<WeatherGridRow[]>("WeatherForecast");
+        }
+    }
+}
