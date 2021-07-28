c3d6606e64111e50254490b40c39a39878769e7a
mag@planetfwd.com <mag@planetfwd.com>
2021-07-28 15:46:27 -0700

    implement weather grid

--- WeatherDashboard/Client/Program.cs
+++ WeatherDashboard/Client/Program.cs
@@ -19,6 +19,8 @@ namespace WeatherDashboard.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            builder.Services.AddTelerikBlazor();
+
             await builder.Build().RunAsync();
         }
     }
