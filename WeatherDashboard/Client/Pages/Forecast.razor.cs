cb1e9f7c1c4f877eb7323646b9be55c1edaec38e
Matuts Jiraboondilok <matuts.j@gmail.com>
2021-07-29 10:36:22 -0700

    tweak forecast data structure

--- WeatherDashboard/Client/Pages/Forecast.razor.cs
+++ WeatherDashboard/Client/Pages/Forecast.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -8,14 +9,14 @@ namespace WeatherDashboard.Client.Pages
 {
     public class ForecastBase: ComponentBase
     {
-        protected WeatherGridRow[] Forecasts { get; private set; }
+        protected IEnumerable<WeatherGridRow> Forecasts { get; private set; }
 
         [Inject]
         public HttpClient Client { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            Forecasts = await Client.GetFromJsonAsync<WeatherGridRow[]>("WeatherForecast?" +
+            Forecasts = await Client.GetFromJsonAsync<IEnumerable<WeatherGridRow>>("WeatherForecast?" +
                 "cities=seattle&" +
                 "cities=portland&" +
                 "cities=san francisco");
