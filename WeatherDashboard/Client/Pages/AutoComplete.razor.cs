7565d1127294c981e938c715700db57b37b59f64
Matuts Jiraboondilok <matuts.j@gmail.com>
2021-08-03 13:51:52 -0700

    extract auto complete to new razor component

---
+++ WeatherDashboard/Client/Pages/AutoComplete.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor.Data;
+using WeatherDashboard.Client.Data;
+
+namespace WeatherDashboard.Client.Pages
+{
+    public class AutoCompleteBase : ComponentBase
+    {
+        protected string SearchBox { get; set; }
+
+        protected static Query GetQuery()
+        {
+            return Queries.GetQuery();
+        }
+
+        protected string GetSearchUrl()
+        {
+            return $"https://localhost:5001/LocationSearch?search={SearchBox}";
+        }
+
+        protected void OnAutoCompleteKeyPressed(KeyboardEventArgs args)
+        {
+            SearchBox += args.Key;
+        }
+
+        protected void OnAutoCompleteKeyDown(KeyboardEventArgs args)
+        {
+            if (args.Code == "Backspace" && SearchBox.Length > 0)
+            {
+                SearchBox = SearchBox.Remove(SearchBox.Length - 1);
+            }
+        }
+    }
+}
---
+++ WeatherDashboard/Client/Pages/DataGrid.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using WeatherDashboard.Client.ViewModels;
+
+namespace WeatherDashboard.Client.Pages
+{
+    public class DataGridBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        protected string City { get; set; }
+
+        protected ObservableCollection<WeatherGridRow> Forecasts { get; private set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Forecasts = await Client.GetFromJsonAsync<ObservableCollection<WeatherGridRow>>("WeatherForecast/GetMultiple?" +
+                "cities=seattle&" +
+                "cities=portland&" +
+                "cities=san francisco");
+        }
+
+        protected async void OnAddClicked()
+        {
+            var forecast = await Client.GetFromJsonAsync<WeatherGridRow>($"WeatherForecast/Get?city={City}");
+            Forecasts.Add(forecast);
+        }
+    }
+}
