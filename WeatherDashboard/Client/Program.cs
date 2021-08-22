3c3a12206fe2f96f9976fc0da0b0c7379dacc92e
mag@planetfwd.com <mag@planetfwd.com>
2021-08-21 21:19:19 -0700

    update autocomplete

--- WeatherDashboard/Client/Program.cs
+++ WeatherDashboard/Client/Program.cs
@@ -4,6 +4,7 @@ using Syncfusion.Blazor;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using WeatherDashboard.Client.Services;
 
 namespace WeatherDashboard.Client
 {
@@ -11,8 +12,12 @@ namespace WeatherDashboard.Client
     {
         public static async Task Main(string[] args)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDgxNTgwQDMxMzkyZTMyMmUzMFF5MHFvVkJJczBabEJLWjJHVHl3SVAyclkyMnVGd1VsUE9NajFWOXkzcm89");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddSingleton(typeof(LocationsProvider));
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
