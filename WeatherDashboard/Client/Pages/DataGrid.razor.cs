378894e8b990628891e44cb63a72dab090038202
mag@planetfwd.com <mag@planetfwd.com>
2021-08-23 10:44:43 -0700

    demo updates

--- WeatherDashboard/Client/Pages/DataGrid.razor.cs
+++ WeatherDashboard/Client/Pages/DataGrid.razor.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor.Data;
 using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using WeatherDashboard.Client.ViewModels;
+using WeatherDashboard.Shared.Data;
 
 namespace WeatherDashboard.Client.Pages
 {
@@ -15,6 +18,31 @@ namespace WeatherDashboard.Client.Pages
         protected string City { get; set; }
 
         protected ObservableCollection<WeatherGridRow> Forecasts { get; private set; }
+
+        protected string RemoteSearch { get; set; }
+
+        protected static Query GetQuery()
+        {
+            return Queries.GetQuery();
+        }
+
+        protected string GetSearchUrl()
+        {
+            return $"https://localhost:5001/LocationSearch?search={RemoteSearch}";
+        }
+
+        protected void OnAutoCompleteKeyPressed(KeyboardEventArgs args)
+        {
+            RemoteSearch += args.Key;
+        }
+
+        protected void OnAutoCompleteKeyDown(KeyboardEventArgs args)
+        {
+            if (args.Code == "Backspace" && RemoteSearch.Length > 0)
+            {
+                RemoteSearch = RemoteSearch.Remove(RemoteSearch.Length - 1);
+            }
+        }
 
         protected override async Task OnInitializedAsync()
         {
